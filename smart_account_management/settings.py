a326bb5731afa35d84e597e0c751fa32e97299c7
Sefa-Keles <sefakeles.uk@gmail.com>
2026-03-08 04:01:50 +0000

    Refactor SECRET_KEY retrieval to use os.environ.get for production security

--- smart_account_management/settings.py
+++ smart_account_management/settings.py
@@ -24,10 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(
-    'SECRET_KEY',
-    'django-insecure-change-me-before-production',
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
