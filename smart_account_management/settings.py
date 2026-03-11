958437438eb1d761457dd520fa38a6601305f030
Sefa-Keles <sefakeles.uk@gmail.com>
2026-03-11 15:47:11 +0000

    Add user authentication views and update receipt upload UI

--- smart_account_management/settings.py
+++ smart_account_management/settings.py
@@ -39,10 +39,11 @@ cloudinary.config(
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
+                 '0.0.0.0',
                  '.herokuapp.com',]
 
 
--- smart_account_management/urls.py
+++ smart_account_management/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('receipts/', include('receipts.urls')),
 ]
