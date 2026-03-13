c4bc5b053df24b7d03df1268328bc05774903e37
Sefa-Keles <sefakeles.uk@gmail.com>
2026-03-13 18:04:25 +0000

    Add delete route for receipts in URL configuration

--- receipts/urls.py
+++ receipts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('upload/', views.upload_receipt, name='upload_receipt'),
     path('', views.receipt_list, name='receipt_list'),
     path('<int:receipt_id>/review/', views.receipt_review, name='receipt_review'),
+    path('<int:receipt_id>/delete/', views.receipt_delete, name='receipt_delete'),
     path('<int:receipt_id>/', views.receipt_detail, name='receipt_detail'),
 ]
