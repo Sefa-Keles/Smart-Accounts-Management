a0f4e5a30f4308071f49ae527ef832f772dadb3a
Sefa-Keles <sefakeles.uk@gmail.com>
2026-03-14 01:14:23 +0000

    Add PDF export route for transactions

--- transactions/urls.py
+++ transactions/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.transaction_list, name="transaction_list"),
     path("create/", views.transaction_create, name="transaction_create"),
     path("export/csv/", views.transaction_export_csv, name="transaction_export_csv"),
+    path("export/pdf/", views.transaction_export_pdf, name="transaction_export_pdf"),
     path("<int:transaction_id>/edit/", views.transaction_edit, name="transaction_edit"),
     path("<int:transaction_id>/delete/", views.transaction_delete, name="transaction_delete"),
 ]
