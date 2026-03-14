d8466366332f87d0bf6f318e1ec3b639a88fb3db
Sefa-Keles <sefakeles.uk@gmail.com>
2026-03-14 01:02:07 +0000

    Add URL pattern for transaction creation

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.transaction_list, name="transaction_list"),
+    path("create/", views.transaction_create, name="transaction_create"),
     path("export/csv/", views.transaction_export_csv, name="transaction_export_csv"),
     path("<int:transaction_id>/edit/", views.transaction_edit, name="transaction_edit"),
     path("<int:transaction_id>/delete/", views.transaction_delete, name="transaction_delete"),
